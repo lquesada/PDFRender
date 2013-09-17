3b17a182e1ab0067c33197a56afceea462b18633
Luis Quesada <luis@elezeta.com>
2013-09-17 19:15:01 +0200

    Fixed bug: would render only to the first resolution specified

--- pdfrender.sh
+++ pdfrender.sh
@@ -45,7 +45,7 @@ echo ""
 
 for i in $@;do
   echo "Generating `echo $file | sed s/'.pdf'//Ig`-$i.pdf file"
-  convert `ls $temp/$file-expanded*.png | sort -V` -scale $1! `echo $file | sed s/'.pdf'//g`-$i.pdf 
+  convert `ls $temp/$file-expanded*.png | sort -V` -scale $i! `echo $file | sed s/'.pdf'//g`-$i.pdf 
   echo "Generated"
   echo ""
 done
